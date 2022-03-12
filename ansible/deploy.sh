889beb85d611025c35c01f95e650d0b6b86c2eb7
Sara Vidal <sara.vidal.f@gmail.com>
2022-03-12 10:50:53 +0100

    ansible config nfs

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,1 +1,2 @@
+ansible-playbook -i inventory set_nfs.yaml
 ansible-playbook -i inventory set_k8s.yaml
--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,26 +1,39 @@
-
-
-
 # Update Ansible inventory
 resource "local_file" "ansible_host" {
   depends_on = [
-    azurerm_linux_virtual_machine.myterraformvm[0]
+    azurerm_linux_virtual_machine.myterraformvm[0],
+    azurerm_linux_virtual_machine.myterraformvm[1],
+    azurerm_linux_virtual_machine.myterraformvm[2],
+    azurerm_linux_virtual_machine.myterraformvm[3],
   ]
   content  = "[all:vars]\nansible_python_interpreter=/usr/bin/python3\nansible_user=${var.ssh_user}\nansible_ssh_common_args='-o StrictHostKeyChecking=no'\n\n[master]\n${azurerm_linux_virtual_machine.myterraformvm[0].public_ip_address}\n\n[workers]\n${azurerm_linux_virtual_machine.myterraformvm[1].public_ip_address}\n${azurerm_linux_virtual_machine.myterraformvm[2].public_ip_address}\n\n[nfs]\n${azurerm_linux_virtual_machine.myterraformvm[3].public_ip_address}"
   filename = "../ansible/inventory"
 }
 
+# Generate group_var for public ips
+resource "local_file" "public_ips" {
+  depends_on = [
+    azurerm_linux_virtual_machine.myterraformvm[0],
+    azurerm_linux_virtual_machine.myterraformvm[1],
+    azurerm_linux_virtual_machine.myterraformvm[2],
+    azurerm_linux_virtual_machine.myterraformvm[3],
+  ]
+  content  = "---\nmaster_public_ip:${azurerm_linux_virtual_machine.myterraformvm[0].public_ip_address}\nworker1_public_ip:${azurerm_linux_virtual_machine.myterraformvm[1].public_ip_address}\nworker2_public_ip:${azurerm_linux_virtual_machine.myterraformvm[2].public_ip_address}\nnfs_public_ip:${azurerm_linux_virtual_machine.myterraformvm[3].public_ip_address}"
+  filename = "../ansible/group_vars/public_ips"
+}
+
 # Upload Ansible playbook 
 resource "null_resource" "null1" {
   depends_on = [
-    local_file.ansible_host
+    local_file.ansible_host,
+    local_file.public_ips
   ]
 
   # waits vm to setup
   provisioner "local-exec" {
     command = "sleep 30"
   }
-  # uploads all content from ansible to folder to master
+  # uploads all content from ansible folder to master
   provisioner "local-exec" {
     command = "scp -r -o StrictHostKeyChecking=no -i ${var.private_key_path} ../ansible ${var.ssh_user}@${azurerm_linux_virtual_machine.myterraformvm[0].public_ip_address}:/home/${var.ssh_user}"
   }
